86d473fb05e33bbfdcc386d134e62927f408b1d9
Richard Harrington <harrington.richard@gmail.com>
2016-08-01 12:02:50 -0400

    got user input working

--- src/income_converter/core.cljs
+++ src/income_converter/core.cljs
@@ -9,6 +9,20 @@
 (enable-console-print!)
 
 
+;; define your app data so that it doesn't get over-written on reload
+
+(def initial-state {:hours-per-week 30
+                    :weeks-off 4
+                    :monthly-health-ins-diff 200
+
+                    :min-hourly-wage 5
+                    :max-hourly-wage 205
+                    :hourly-wage-inc 5})
+
+(defonce app-state (atom initial-state))
+
+
+
 (def soc-sec-rate 0.123)
 (def medicare-rate 0.030)
 (def soc-sec-salary-cutoff 113700)
@@ -34,57 +48,99 @@
       (for [n [hourly-wage weekly-income yearly-income if-w2 if-1099]]
         [:td (dollar-str n)])])))
 
-(defn main-table [{:keys [min-hourly-wage
+(defn main-table [{:keys [hours-per-week
+                          weeks-off
+                          monthly-health-ins-diff
+
+                          min-hourly-wage
                           max-hourly-wage
-                          hourly-wage-inc] :as props}]
+                          hourly-wage-inc]}]
   (sab/html
    [:table
-    [:tr
-     [:th "hourly wage"]
-     [:th "weekly income"]
-     [:th "yearly income"]
-     [:th "If contractor gets W-2, FTE salary equiv is:"]
-     [:th "If contractor gets 1099, FTE salary equiv is:"]]
-    (map #(row (assoc props :hourly-wage %))
-         (range min-hourly-wage max-hourly-wage hourly-wage-inc))]))
+    [:thead
+     [:tr
+      [:th "hourly wage"]
+      [:th "weekly income"]
+      [:th "yearly income"]
+      [:th "If contractor gets W-2, FTE salary equiv is:"]
+      [:th "If contractor gets 1099, FTE salary equiv is:"]]]
+    [:tbody
+     (map #(row {:hourly-wage %
+                 :hours-per-week hours-per-week
+                 :weeks-off weeks-off
+                 :monthly-health-ins-diff monthly-health-ins-diff})
+          (range min-hourly-wage max-hourly-wage hourly-wage-inc))]]))
 
-(defn input-component [{:keys [hours-per-week
-                               weeks-off
-                               monthly-health-ins-diff]}]
+(defn input-row [{:keys [val display update!]}]
   (sab/html
-   [:input {:onChange #(println (aget % "target" "value"))}]
-   [:div "input something here"]))
+   [:div.input-row
+    [:input {:value val :on-change #(update! (aget % "target" "value"))}]
+    [:label display]]))
+
+(defn input-component [{:keys [update-app-state!
+
+                               hours-per-week
+                               weeks-off
+                               monthly-health-ins-diff
+
+                               min-hourly-wage
+                               max-hourly-wage
+                               hourly-wage-inc]}]
+  (println hours-per-week)
+  (sab/html
+   [:div.input-section
+    (map input-row [{:val hours-per-week
+                     :display "Hours per week"
+                     :update! (partial update-app-state! :hours-per-week)}
+                    {:val weeks-off
+                     :display "Weeks off"
+                     :update! (partial update-app-state! :weeks-off)}
+                    {:val monthly-health-ins-diff
+                     :display "Differential in monthly health insurance payment"
+                     :update! (partial update-app-state! :monthly-health-ins-diff)}
+                    {:val min-hourly-wage
+                     :display "Minimum hourly wage in table"
+                     :update! (partial update-app-state! :min-hourly-wage)}
+                    {:val max-hourly-wage
+                     :display "Maximum hourly wage"
+                     :update! (partial update-app-state! :max-hourly-wage)}
+                    {:val hourly-wage-inc
+                     :display "Amount hourly wage goes up in each row"
+                     :update! (partial update-app-state! :hourly-wage-inc)}])]))
+
+
+(defn validate-input [input]
+  (if (= input "")
+    0
+    (js/parseInt input)))
+
+(declare render)
 
 (defn page [props]
   (sab/html
    [:div.page
-    [:h1 "hi"]
-    (input-component {})
+    [:h1 "Income conversion chart"]
+    (input-component (assoc props :update-app-state!
+                            (fn [key val]
+                              (when-let [n (validate-input val)]
+                                (swap! app-state assoc key n)
+                                (render)))))
     (main-table props)]))
 
 
-;; define your app data so that it doesn't get over-written on reload
+(defn render []
+  (println @app-state)
 
-(def initial-state {:hours-per-week 30
-                    :weeks-off 4
-                    :monthly-health-ins-diff 200
+  (let [node (.getElementById js/document "app")]
+    (.render js/React (page @app-state) node)))
 
-                    :min-hourly-wage 5
-                    :max-hourly-wage 205
-                    :hourly-wage-inc 5})
+(render)
 
-(defonce app-state (atom initial-state))
+
 
 (defn on-js-reload []
   #_(reset! app-state initial-state)
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
-
-(defn render []
-
-  (let [node (.getElementById js/document "app")]
-    (.render js/React (page @app-state) node)))
-
-(render)
+  )
